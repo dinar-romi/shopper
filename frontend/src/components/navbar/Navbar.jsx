6eb58d3a5e53ed9efa69fe784053ae987c577141
dinar-romi <dinarromi.pc@gmail.com>
2024-04-02 15:56:36 +0700

    add hero section

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import "./navbar.css";
-import logo from "../assets/logo.png";
-import cart_icon from "../assets/cart_icon.png";
+import { useState } from 'react';
+import './navbar.css';
+import logo from '../assets/logo.png';
+import cart_icon from '../assets/cart_icon.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("shop");
+  const [menu, setMenu] = useState('shop');
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -13,10 +14,31 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>Shop {menu === "shop" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("mens")}>Men {menu === "mens" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("womens")}>Women {menu === "womens" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("kids")}>Kids {menu === "kids" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu('shop')}>
+          <Link style={{ textDecoration: 'none' }} to="/">
+            Shop
+          </Link>{' '}
+          {menu === 'shop' ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu('mens')}>
+          <Link style={{ textDecoration: 'none' }} to="/mens">
+            Men
+          </Link>{' '}
+          {menu === 'mens' ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu('womens')}>
+          <Link style={{ textDecoration: 'none' }} to="womens">
+            Women
+          </Link>{' '}
+          {menu === 'womens' ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu('kids')}>
+          {' '}
+          <Link style={{ textDecoration: 'none' }} to="/kids">
+            Kids
+          </Link>{' '}
+          {menu === 'kids' ? <hr /> : <></>}
+        </li>
       </ul>
 
       <div className="nav-login-cart">
